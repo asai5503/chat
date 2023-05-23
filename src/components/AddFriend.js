4010623c666df77d0b819c480aafe2d26d8ddf05
yuuki5503 <b.ya.gogo.03.bmt@gmail.com>
2023-05-23 20:07:23 +0900

    フレンドのブロックを追加

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   doc,
   setDoc,
@@ -14,6 +14,12 @@ const AddFriend = () => {
   const [username, setUsername] = useState("");
   const [error, setError] = useState("");
   const [userList, setUserList] = useState([]);
+  const [friends, setFriends] = useState([]);
+  const [blockedUsers, setBlockedUsers] = useState([]);
+
+  useEffect(() => {
+    fetchFriendsAndBlockedUsers();
+  }, []);
 
   const searchUser = async () => {
     // friendユーザを探す
@@ -36,30 +42,116 @@ const AddFriend = () => {
     const userSnapshot = await getDoc(userRef);
     const userData = userSnapshot.data();
 
-    if (userData.private && userData.private.friends && userData.private.friends.includes(friendId)) {
+    if (
+      userData.private &&
+      userData.private.friends &&
+      userData.private.friends.includes(friendId)
+    ) {
       setError("This user is already your friend.");
       return;
     }
 
-    const newFriends = userData.private && userData.private.friends
-      ? [...userData.private.friends, friendId]
-      : [friendId];
-      
-      await setDoc(userRef, { 
-        ...userData, 
-        private: {
-          ...userData.private,
-          friends: newFriends 
-        }
-      });
+    const newFriends =
+      userData.private && userData.private.friends
+        ? [...userData.private.friends, friendId]
+        : [friendId];
+
+    await setDoc(userRef, {
+      ...userData,
+      private: {
+        ...userData.private,
+        friends: newFriends,
+      },
+    });
 
     setError("Friend added successfully.");
-    searchUser(); // to refresh the user list
+    setUserList();
+    fetchFriendsAndBlockedUsers();
+  };
+
+  //フレンドのブロック
+  const blockFriend = async (friendId) => {
+    // 自分のユーザ情報を取得
+    const userRef = doc(db, "users", auth.currentUser.uid);
+    const userSnapshot = await getDoc(userRef);
+    const userData = userSnapshot.data();
+
+    if (!userData.private.friends.includes(friendId)) {
+      setError("This user is not your friend.");
+      return;
+    }
+
+    const newFriends = userData.private.friends.filter((id) => id !== friendId);
+    const newBlockedUsers = userData.private.blockedUser
+      ? [...userData.private.blockedUser, friendId]
+      : [friendId];
+
+    await setDoc(userRef, {
+      ...userData,
+      private: {
+        ...userData.private,
+        friends: newFriends,
+        blockedUser: newBlockedUsers,
+      },
+    });
+
+    setError("Friend blocked successfully.");
+    setUserList();
+    fetchFriendsAndBlockedUsers();
+  };
+
+  const fetchFriendsAndBlockedUsers = async () => {
+    const userRef = doc(db, "users", auth.currentUser.uid);
+    const userSnapshot = await getDoc(userRef);
+    const userData = userSnapshot.data();
+
+    const friendsIds = userData?.private?.friends || [];
+    const blockedUsersIds = userData?.private?.blockedUser || [];
+
+    const friendsPromises = friendsIds.map((friendId) =>
+      getDoc(doc(db, "users", friendId))
+    );
+    const friendsDocs = await Promise.all(friendsPromises);
+    setFriends(
+      friendsDocs.map((doc) => ({ id: doc.id, name: doc.data().name }))
+    );
+
+    const blockedUsersPromises = blockedUsersIds.map((userId) =>
+      getDoc(doc(db, "users", userId))
+    );
+    const blockedUsersDocs = await Promise.all(blockedUsersPromises);
+    setBlockedUsers(
+      blockedUsersDocs.map((doc) => ({ id: doc.id, name: doc.data().name }))
+    );
+  };
+
+  const unblockUser = async (userId) => {
+    console.log("Unblock button clicked for userId: ", userId);
+    // Get current user's data
+    const userRef = doc(db, "users", auth.currentUser.uid);
+    const userSnapshot = await getDoc(userRef);
+    const userData = userSnapshot.data();
+
+    // Filter out the unblocked user from blockedUsers
+    const newBlockedUsers = userData.private.blockedUser.filter(
+      (id) => id !== userId
+    );
+
+    await setDoc(userRef, {
+      ...userData,
+      private: {
+        ...userData.private,
+        blockedUser: newBlockedUsers,
+      },
+    });
+
+    setError("User unblocked successfully.");
+    setUserList();
+    fetchFriendsAndBlockedUsers();
   };
 
   return (
     <div>
-      <h2>Add Friend</h2>
       <input
         type="text"
         placeholder="Enter friend's username"
@@ -68,13 +160,36 @@ const AddFriend = () => {
       />
       <button onClick={searchUser}>Search</button>
       {error && <p>{error}</p>}
-      {userList.map((user) => (
+
+      {userList?.map((user) => (
         <div key={user.id}>
-          <img src={user.data.iconUrl} alt={user.data.name} />
+          <img
+            src={user.data.iconUrl}
+            alt={user.data.name}
+            style={{ width: "100px", height: "100px", objectFit: "cover" }}
+          />
           <span>{user.data.name}</span>
           <button onClick={() => addFriend(user.id)}>Add</button>
         </div>
       ))}
+      <div>
+        <h2>Friends</h2>
+        {friends?.map((friend, index) => (
+          <div key={index}>
+            {friend.name}
+            <button onClick={() => blockFriend(friend.id)}>Block</button>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Blocked Users</h2>
+        {blockedUsers?.map((user, index) => (
+          <div key={index}>
+            {user.name}
+            <button onClick={() => unblockUser(user.id)}>Unblock</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
